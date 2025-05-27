2caec92557ab53b40e9b583a74200ffc043d2859
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-05-26 22:47:40 -0300

    feat: Adiciona model e migration de message

---
+++ database/migrations/2025_05_27_002549_create_messages_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * @var string
+     */
+    protected $connection = 'mysql';
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->text('text');
+            $table->boolean('follower');
+            $table->unsignedBigInteger('sender_id');
+            $table->unsignedBigInteger('receiver_id');
+            $table->timestamps();
+            $table->foreign('sender_id')->references('id')->on('users');
+            $table->foreign('receiver_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->dropForeign('messages_sender_id_foreign');
+            $table->dropForeign('messages_receiver_id_foreign');
+        });
+        Schema::dropIfExists('messages');
+    }
+};
---
+++ database/migrations/2025_05_27_010055_create_image_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * @var string
+     */
+    protected $connection = 'mysql';
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('image_url', 80)->nullable();
+            $table->unsignedBigInteger('message_id')->nullable();
+            $table->timestamps();
+            $table->foreign('message_id')->references('id')->on('messages');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->dropForeign('images_message_id_foreign');
+        });
+        Schema::dropIfExists('images');
+    }
+};
