2dc518d41d74e2656a3ee9f4e141924767620eb7
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-05-27 22:35:39 -0300

    feat: Adiciona os relacionamentos de Event e Address

---
+++ database/migrations/2025_05_28_001509_create_user_event_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_event', function (Blueprint $table) {
+            $table->id();
+            $table->charset('utf8mb4');
+            $table->collation('utf8mb4_unicode_ci');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('event_id');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('event_id')->references('id')->on('events');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user_event', function (Blueprint $table) {
+            $table->dropForeign('user_event_user_id_foreign');
+            $table->dropForeign('user_event_event_id_foreign');
+        });
+        Schema::dropIfExists('user_event');
+    }
+};
