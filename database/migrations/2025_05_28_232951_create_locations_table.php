03fa8a46c064968f0086d29b73ad98a9bf35468f
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-05-28 22:16:24 -0300

    feat: Adiciona model e migration de Location

---
+++ database/migrations/2025_05_28_232951_create_locations_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     *@var string
+     */
+    protected $connection = 'mysql';
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->charset('utf8mb4');
+            $table->collation('utf8mb4_unicode_ci');
+            $table->string('country', 70);
+            $table->string('state', 70)->nullable();
+            $table->unsignedBigInteger('post_id');
+            $table->timestamps();
+            $table->foreign('post_id')->references('id')->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('locations', function (Blueprint $table) {
+            $table->dropForeign('locations_post_id_foreign');
+        });
+        Schema::dropIfExists('locations');
+    }
+};
