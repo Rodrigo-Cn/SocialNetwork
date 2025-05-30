a0e989711dd7c47cb7e7f2c1f4218cb19d63c0af
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-05-29 21:22:29 -0300

    feat: Adiciona model e migration de Like

---
+++ database/migrations/2025_05_28_233052_create_likes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * @var string
+     */
+    protected $connection = 'mysql';
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->charset('utf8mb4');
+            $table->collation('utf8mb4_unicode_ci');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('likeable_id');
+            $table->string('likeable_type');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->index(['likeable_id','likeable_type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('likes', function (Blueprint $table) {
+            $table->dropForeign('likes_user_id_foreign');
+        });
+        Schema::dropIfExists('likes');
+    }
+};
