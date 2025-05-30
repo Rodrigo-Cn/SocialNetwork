725f6fc14d8914e2ec469e7cc444eb23e7031309
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-05-29 21:22:43 -0300

    feat: Adiciona model e migration de Comment

---
+++ database/migrations/2025_05_28_233034_create_comments_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * @var string
+     */
+    protected $connection = 'mysql';
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->charset('utf8mb4');
+            $table->collation('utf8mb4_unicode_ci');
+            $table->text('text');
+            $table->string('image_url', 255)->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('post_id');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('post_id')->references('id')->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropForeign('comments_user_id_foreign');
+            $table->dropForeign('comments_post_id_foreign');
+        });
+        Schema::dropIfExists('comments');
+    }
+};
