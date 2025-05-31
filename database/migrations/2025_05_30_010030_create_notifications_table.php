7046b180fef26fc4eb2423f14742545ab0e69046
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-05-30 23:02:54 -0300

    feat: Adiciona model e migration de Notification

---
+++ database/migrations/2025_05_30_010030_create_notifications_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * @var string
+     */
+    protected $connection = 'mysql';
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->charset('utf8mb4');
+            $table->collation('utf8mb4_unicode_ci');
+            $table->text('description');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            $table->dropForeign('notifications_user_id_foreign');
+        });
+        Schema::dropIfExists('notifications');
+    }
+};
