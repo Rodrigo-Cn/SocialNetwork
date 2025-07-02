508b772699f815936fea2c7bba174bc3891d8ff8
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-07-01 21:51:09 -0300

    feat: Adiciona as rotas de autenticação

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\GenderController;
 use App\Http\Controllers\MaritalStatusController;
@@ -9,6 +10,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('auth')->group(function () {
+    Route::post('register/', [AuthController::class, 'register']);
+    Route::post('login/', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
 
 Route::apiResource('maritalstatus', MaritalStatusController::class)
     ->only(['index', 'show'])
