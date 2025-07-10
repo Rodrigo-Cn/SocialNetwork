e7c4eb961f315c38e84cb8f0b784ded16805404c
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-07-09 23:06:17 -0300

    feat: Adiciona logout na aplicação

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,8 @@ use App\Http\Requests\AuthRequest;
 use App\Repositories\Contracts\UserRepositoryInterface;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class AuthController extends Controller
 {
@@ -20,23 +22,52 @@ class AuthController extends Controller
     {
         $params = $request->validated();
 
-        if(Auth::attempt($params)){
-            $user = $this->userRepository->findByEmail($params['email']);
-            $token = $user->createToken('auth_token');
-            return response()->json([
-                'success' => true,
-                'token' => $token->plainTextToken
-            ]);
-        }
+        try {
+            if (Auth::attempt($params)) {
+                $user = $this->userRepository->findByEmail($params['email']);
 
-        return response()->json([
-            'success' => false,
-            'message' => 'E-mail ou senha inválidos.'
-        ], 401);
+                if (!$user) {
+                    return response()->json([
+                        'success' => false,
+                        'message' => 'Usuário não encontrado.'
+                    ], 404);
+                }
+
+                $token = $user->createToken('auth_token');
+
+                return response()->json([
+                    'success' => true,
+                    'token' => $token->plainTextToken
+                ]);
+            }
+
+            return response()->json([
+                'success' => false,
+                'message' => 'E-mail ou senha inválidos.'
+            ], 401);
+
+        } catch (Throwable $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Ocorreu um erro interno. Contate o suporte.'
+            ], 500);
+        }
     }
 
     public function logout(Request $request)
     {
+        try {
+            $request->user()->currentAccessToken()->delete();
 
+            return response()->json([
+                'success' => true,
+                'message' => 'Logout realizado com sucesso.'
+            ]);
+        } catch (Throwable $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Ocorreu um erro interno. Contate o suporte.'
+            ], 500);
+        }
     }
 }
